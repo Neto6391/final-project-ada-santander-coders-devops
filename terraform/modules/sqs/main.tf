750f3859edd0c8d78b39a445e3c34a14e0b67a48
Neto6391 <neto6391@gmail.com>
2024-12-07 15:50:02 -0300

    fix: fixes errors

--- terraform/modules/sqs/main.tf
+++ terraform/modules/sqs/main.tf
@@ -6,8 +6,8 @@ resource "aws_sqs_queue" "notify_queue" {
   name = "${var.prefix}-notify-queue"
 }
 
-resource "aws_sqs_queue_policy" "queue_policy" {
-  queue_url = aws_sqs_queue.my_queue.id
+resource "aws_sqs_queue_policy" "notify_queue_policy" {
+  queue_url = aws_sqs_queue.notify_queue.id
   policy    = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -21,7 +21,28 @@ resource "aws_sqs_queue_policy" "queue_policy" {
           "sqs:ReceiveMessage",
           "sqs:DeleteMessage"
         ],
-        Resource = aws_sqs_queue.my_queue.arn
+        Resource = aws_sqs_queue.notify_queue.arn
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "process_queue_policy" {
+  queue_url = aws_sqs_queue.process_queue.id
+  policy    = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage"
+        ],
+        Resource = aws_sqs_queue.process_queue.arn
       }
     ]
   })
