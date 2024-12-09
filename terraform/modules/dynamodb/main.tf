3d53f949fc3a7fdfe7ddb3e0cba45c87137af951
Neto6391 <neto6391@gmail.com>
2024-12-09 20:51:32 -0300

    refactor: remove rds to use dynamoDB

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -1,7 +1,12 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
   name           = var.table_name
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = var.hash_key
+  hash_key       = "id"               
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
 
   dynamic "attribute" {
     for_each = var.attributes
